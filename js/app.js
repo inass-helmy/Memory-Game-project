4c3740e13526ff3b81988aad0ff45c10e0d1d75e
Enas Samir <enassamir@ACA8019E.ipt.aol.com>
2018-03-27 07:51:57 +0200

    add timer and manage the congratulations message

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-
- // Create a list that holds all of deck's cards
- 
+// Create a list that holds all of deck's cards
 let card = document.querySelectorAll(".card");
 let cardList = [...card];
 let shuffledCards;
 let deck = document.querySelector(".deck");
+
 // counter for the user clicks
 let moves = document.querySelector(".moves");
 let count;
@@ -12,7 +11,6 @@ var time;
 finalMoves = document.querySelector(".finalMoves");
 
 //time counter
-
 let timer = document.querySelector(".timer");
 let finalTime = document.querySelector(".finalTime");
 
@@ -25,6 +23,7 @@ let secondStar = document.querySelector("#secondStar");
 let thirdStar = document.querySelector("#thirdStar");
 let starRate = document.querySelector("#starRate");
 let stars = document.querySelector(".stars")
+
 //the congratulation message
 let congrats = document.querySelector(".congMessage");
 
@@ -53,7 +52,7 @@ function startGame() {
 
         // display and check if a two cards was clicked matched or not
         shuffledCards[i].addEventListener("click", function display() {
-        	displayMove();
+            displayMove();
             this.classList.add("open");
             this.classList.add("show");
             this.classList.add("no-click");
@@ -75,15 +74,16 @@ function startGame() {
     });
 }
 
+// set the timer, the moves counter, empty the opened cards array and remove all classes added throughout the last game
 function setGame() {
-    
-    congrats.style.display = "none";
+
+    // congrats.style.display = "none";
     timer.textContent = "00:00";
     clearInterval(time);
     setTimer();
     count = 0;
-     moves.innerHTML = count;
-     openedList = [];
+    moves.innerHTML = count;
+    openedList = [];
     thirdStar.style.display = "initial";
     secondStar.style.display = "initial";
     for (let i = 0; i < shuffledCards.length; i++) {
@@ -94,14 +94,14 @@ function setGame() {
 
 //@Description to check the two cards match or not and disable all other cards
 function checkMatch(openedCard) {
-openedList = deck.querySelectorAll(".open");
+    openedList = deck.querySelectorAll(".open");
     if (openedList.length === 2) {
         for (var i = 0; i < shuffledCards.length; i++) {
             shuffledCards[i].classList.add("no-click");
         }
         if (openedList[0].type === openedList[1].type) {
             matchedCards();
-                gameEnd();
+            gameEnd();
 
 
         } else {
@@ -120,7 +120,7 @@ function matchedCards() {
     for (var i = 0; i < shuffledCards.length; i++) {
         shuffledCards[i].classList.remove("no-click");
     }
-   matchCards = deck.querySelectorAll(".match");
+    matchCards = deck.querySelectorAll(".match");
 
 }
 //  if the two cards are unmatched flip the cards and set a delay for 0.7s 
@@ -138,12 +138,6 @@ function unmatchedCards() {
     }, 700);
 
 }
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -163,13 +157,13 @@ function shuffle(array) {
 
 //@Description to count the moves of the users and displays it on screen
 function displayMove() {
-        count++;
-        moves.innerHTML = count;
+    count++;
+    moves.innerHTML = count;
     if (count > 30 && count <= 40) {
-        thirdStar.style.display = "none";
+        thirdStar.style.color = "#404040";
     }
     if (count > 40) {
-        secondStar.style.display = "none";
+        secondStar.style.color = "#404040";
 
     }
 }
@@ -177,33 +171,29 @@ function displayMove() {
 //@Description to set the timer and display it on the screen
 function setTimer() {
     let secUnits = 0;
-let secTenth = 0;
-let minUnits = 0;
-let minTenth = 0;
+    let secTenth = 0;
+    let minUnits = 0;
+    let minTenth = 0;
     time = setInterval(function() {
-        if(openedList.length>=1){
-        	secUnits++
-        	if(secUnits>9) {
-        	secUnits = 0;
-        		secTenth++;
-        		if (secUnits === 0 && secTenth === 6) {
-            secUnits = 0;
-    		secTenth = 0;
-            minUnits++;
-            if(minUnits>9) {
-        	minUnits = 0;
-        		minTenth++;
-        		minUnits++;
-        	}
-        	}
-
-        
-
+        if (openedList.length >= 1) {
+            secUnits++
+            if (secUnits > 9) {
+                secUnits = 0;
+                secTenth++;
+                if (secUnits === 0 && secTenth === 6) {
+                    secUnits = 0;
+                    secTenth = 0;
+                    minUnits++;
+                    if (minUnits > 9) {
+                        minUnits = 0;
+                        minTenth++;
+                        minUnits++;
+                    }
+                }
+            }
+            timer.textContent = minTenth + minUnits + ":" + secTenth + secUnits;
         }
-		timer.textContent = minTenth + minUnits + ":" + secTenth + secUnits;
 
-        }
-        
     }, 1000);
 }
 
@@ -213,15 +203,19 @@ function gameEnd() {
         finalTime.textContent = timer.innerHTML;
         finalMoves.innerHTML = count;
         clearInterval(time);
+        for (var i = 0; i < shuffledCards.length; i++) {
+            shuffledCards[i].classList.add("no-click");
+            shuffledCards[i].classList.remove("show", "open", "match");
+        }
 
         congrats.style.display = "initial";
         var finalStars = document.querySelector(".stars").innerHTML;
         starRate.innerHTML = finalStars;
 
         playAgain.addEventListener("click", function() {
- 		shuffledCards = shuffle(cardList);
+            shuffledCards = shuffle(cardList);
 
-    setGame();
+            setGame();
         });
 
         thanks.addEventListener("click", function() {
@@ -229,24 +223,8 @@ function gameEnd() {
             moves.innerHTML = 0;
             timer.textContent = "00:00";
 
-            for (var i = 0; i < shuffledCards.length; i++) {
-            shuffledCards[i].classList.add("no-click");
-            shuffledCards[i].classList.remove("show","open","match");
-        }
         });
 
     }
 
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
